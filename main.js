5693d8eaee1d1c1143487ceb213c755484cfde5b
MorninDue <100368985+MorninDue@users.noreply.github.com>
2023-06-26 18:27:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parents('canvas');
+	
+	instializeInSetup(ping-pong);
+
+	video = createCapture(VIDEO);
+	video.size(800,300);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('Model Loaded!');
+}
+
+function gotPoses(results)
+{
+	if(results.length > 0)
+	{
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX +", noseY = " +noseY)
+	}
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
